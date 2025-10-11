9ae2cde1b159fbea511e545c368d17f9fab0db55
kmp alpha <kmpalpha@gmail.com>
2025-10-11 23:26:14 +0700

    feature attendance api

---
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Attendance;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AttendanceController extends Controller
+{
+    public function checkin(Request $request)
+    {
+        return response()->json(['message' => 'Check-in OK']);
+    }
+    // public function Checkin(Request $request)
+    // {
+    //     $user = $request->user();
+    //     $employee = $user->employee;
+    //     $todayCheckin = Attendance::where('employee_id', $employee->employee_id)
+    //         ->whereDate('created_at', Carbon::today())
+    //         ->first();
+
+    //     if (!$employee) {
+    //         return response()->json([
+    //             'message' => 'User tidak terhubung dengan employee'
+    //         ], 400);
+    //     }
+
+    //     $todayCheckin = Attendance::where('employee_id', $employee->employee_id)
+    //         ->whereDate('created_at', Carbon::today())
+    //         ->first();
+
+    //     if ($todayCheckin) {
+    //             return response()->json([
+    //                 'message' => 'Anda sudah check-in hari ini',
+    //             ], 400);
+    //         }
+
+    //     $attendance = Attendance::create([
+    //         'employee_id' => $employee->employee_id,
+    //         'checkin_time' => Carbon::now(),
+    //     ]);
+
+    //     return response()->json([
+    //         'message' => 'Check-in berhasil',
+    //         'data' => $attendance
+    //     ]);
+    // }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/attendance/checkin', [AttendanceController::class, 'checkin']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/attendance/checkin', [AttendanceController::class, 'checkin']);
 });
