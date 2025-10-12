b629d9043e6013e37937bed3f039cfd16707f396
kmp alpha <kmpalpha@gmail.com>
2025-10-12 12:11:56 +0700

    fix API attendance

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
 
-    Route::get('/attendance/checkin', [AttendanceController::class, 'checkin']);
-    Route::get('/attendance/checkout', [AttendanceController::class, 'checkout']);
+    Route::post('/attendance/checkin', [AttendanceController::class, 'checkin']);
+    Route::post('/attendance/checkout', [AttendanceController::class, 'checkout']);
     Route::post('/timesheets', [TimesheetController::class, 'store']);
 });
