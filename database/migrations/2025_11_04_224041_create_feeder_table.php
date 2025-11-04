01b554566ffae4937e44a501d222eda3021fdbe3
kmp alpha <kmpalpha@gmail.com>
2025-11-05 02:36:38 +0700

    feature create migration BAST and API BAST

---
+++ database/migrations/2025_11_04_224041_create_feeder_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::connection('mysql_inventory')->create('FeederDetail', function (Blueprint $table) {
+            $table->id();
+            $table->string('bast_id');
+            $table->foreign('bast_id')->references('bast_id')->on('BastProject')->onDelete('cascade');
+            $table->string('feeder_name')->nullable();
+            $table->string('foto_utara')->nullable();
+            $table->string('foto_barat')->nullable();
+            $table->string('foto_selatan')->nullable();
+            $table->string('foto_timur')->nullable();
+            $table->text('notes')->nullable();    
+            $table->unsignedTinyInteger('progress_percentage')->default(0);
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();      
+            $table->timestamps();
+            
+            $table->unique(['id', 'bast_id']);
+            $table->index('feeder_name');
+            $table->index('created_by');
+            $table->index('updated_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::connection('mysql_inventory')->dropIfExists('FeederDetail');
+    }
+};
