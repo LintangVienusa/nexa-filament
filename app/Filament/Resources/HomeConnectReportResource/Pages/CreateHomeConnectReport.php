17a1fe091ccad1dad973b96abb5cfbbad51fdbea
kmp alpha <kmpalpha@gmail.com>
2025-12-14 01:36:22 +0700

    create homeconnect report resource page

---
+++ app/Filament/Resources/HomeConnectReportResource/Pages/CreateHomeConnectReport.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\HomeConnectReportResource\Pages;
+
+use App\Filament\Resources\HomeConnectReportResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateHomeConnectReport extends CreateRecord
+{
+    protected static string $resource = HomeConnectReportResource::class;
+}
