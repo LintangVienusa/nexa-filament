0d78555fa2b081f4c72e222256240b6c22a725a7
kmp alpha <kmpalpha@gmail.com>
2026-01-12 18:12:48 +0700

    fix double role

--- app/Traits/HasNavigationPolicy.php
+++ app/Traits/HasNavigationPolicy.php
@@ -25,12 +25,7 @@ trait HasNavigationPolicy
             ->lower()
             ->append('.read')
             ->toString();
-// dd(
-//         static::class,
-//         $permission,
-//         $user->can($permission),
-//         $user->getAllPermissions()->pluck('name')->toArray()
-//     );
+
         return $user->can($permission);
     }
 
@@ -43,12 +38,7 @@ trait HasNavigationPolicy
         $tipeEmployee = $user->employee?->employee_type;
         $unitName = strtoupper($user->employee?->organization?->unit_name ?? '');
         
-        // \Log::info('shouldRegisterNavigation called for '.static::class, [
-        //         'user_email' => $user->email ?? 'unknown',
-        //         'job_title' => $jobTitle,
-        //         'unit_name' => $unitName,
-        //         'roles' => $user->roles->pluck('name')->toArray(),
-        //     ]);
+        
         if (!$user) {
             return false;
         }
@@ -62,80 +52,16 @@ trait HasNavigationPolicy
                         \App\Filament\Resources\HomeConnectReportResource::class,
                     ];
                     
-                    return in_array($resourceClass, $allowedResources);
+                    // return in_array($resourceClass, $allowedResources);
+                     return
+                    in_array($resourceClass, $allowedResources)
+                    || static::hasReadPermission();
             }else{
                 return true;
             }
             
 
         }
-
-        // if (in_array($jobTitle, ['CEO','CTO'])) {
-        //    if ($tipeEmployee === 'mitra') {
-        //         $resourceClass = static::class;
-        //         $allowedResources = [
-        //                 \App\Filament\Resources\HomeConnectReportResource::class,
-        //             ];
-                    
-        //             return in_array($resourceClass, $allowedResources);
-        //     }else{
-        //         return true;
-        //     }
-        // }
-
-        // if ($tipeEmployee === 'mitra') {
-        //     $resourceClass = static::class;
-        //     $allowedResources = [
-        //             \App\Filament\Resources\HomeConnectReportResource::class,
-        //         ];
-                
-        //         return in_array($resourceClass, $allowedResources);
-        // }
-
-        // if (in_array($jobTitle, ['VP','Manager','SPV'])) {
-        //     // return $unitName === 'IT' || $unitName === 'HR'; 
-        //      if ($unitName != 'WAREHOUSE') {
-        //         $resourceClass = static::class;
-
-        //         $allowedResources = [
-        //             \App\Filament\Resources\EmployeeResource::class,
-        //             \App\Filament\Resources\AttendanceResource::class,
-        //             \App\Filament\Resources\OvertimeResource::class,
-        //             \App\Filament\Resources\TimesheetResource::class,
-        //             \App\Filament\Resources\PayrollResource::class,
-        //             \App\Filament\Resources\LeaveResource::class,
-        //             \App\Filament\Resources\ProfileResource::class,
-        //             \App\Filament\Resources\UserResource::class,
-        //             // \App\Filament\Resources\MappingRegionResource::class,
-        //         ];
-
-        //         return in_array($resourceClass, $allowedResources);
-        //     }
-        // }
-
-        
-
-        // if ($unitName === 'WAREHOUSE') {
-        //     $resourceClass = static::class;
-
-        //     $allowedResources = [
-        //         \App\Filament\Resources\EmployeeResource::class,
-        //         \App\Filament\Resources\AttendanceResource::class,
-        //         \App\Filament\Resources\OvertimeResource::class,
-        //         \App\Filament\Resources\TimesheetResource::class,
-        //         \App\Filament\Resources\PayrollResource::class,
-        //         \App\Filament\Resources\AssetMovementResource::class,
-        //         \App\Filament\Resources\AssetResource::class,
-        //         \App\Filament\Resources\AssetTransactionResource::class,
-        //         \App\Filament\Resources\InventoryAssetResource::class,
-        //             \App\Filament\Resources\ProfileResource::class,
-        //             \App\Filament\Resources\LeaveResource::class,
-        //             \App\Filament\Resources\UserResource::class,
-        //             // \App\Filament\Resources\MappingRegionResource::class,
-        //     ];
-
-        //     return in_array($resourceClass, $allowedResources);
-        // }
 
         if ( $user->hasRole('employee')) {
             if ($tipeEmployee === 'mitra') {
@@ -144,7 +70,10 @@ trait HasNavigationPolicy
                         \App\Filament\Resources\HomeConnectReportResource::class,
                     ];
                     
-                    return in_array($resourceClass, $allowedResources);
+                    // return in_array($resourceClass, $allowedResources);
+                     return
+                    in_array($resourceClass, $allowedResources)
+                    || static::hasReadPermission();
             }
 
             if ($unitName === 'TECHNICIAN') {
@@ -162,7 +91,10 @@ trait HasNavigationPolicy
                     // \App\Filament\Resources\MappingRegionResource::class,
                 ];
 
-                return in_array($resourceClass, $allowedResources);
+                // return in_array($resourceClass, $allowedResources);
+                 return
+                    in_array($resourceClass, $allowedResources)
+                    || static::hasReadPermission();
             }
 
             if ($unitName != 'WAREHOUSE') {
@@ -177,10 +109,11 @@ trait HasNavigationPolicy
                     \App\Filament\Resources\LeaveResource::class,
                     \App\Filament\Resources\ProfileResource::class,
                     \App\Filament\Resources\UserResource::class,
-                    // \App\Filament\Resources\MappingRegionResource::class,
                 ];
 
-                return in_array($resourceClass, $allowedResources);
+                return
+                    in_array($resourceClass, $allowedResources)
+                    || static::hasReadPermission();
             }
 
             
