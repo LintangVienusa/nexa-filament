47f3870b3893e9e45efaee54bfac2d32417254fe
alphabit-dep <kmpalpha@gmail.com>
2026-03-13 11:53:15 +0700

    improvement app version

---
+++ database/migrations/2026_03_13_111319_create_app_version_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('AppVersion', function (Blueprint $table) {
+            $table->id();
+            $table->string('platform'); 
+            $table->integer('min_build')->default(1);
+            $table->integer('latest_build')->nullable();
+            $table->string('version')->nullable();
+            $table->string('store_url')->nullable();
+            $table->text('update_message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('AppVersion');
+    }
+};
